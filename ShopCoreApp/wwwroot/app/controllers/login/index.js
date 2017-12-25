808830ca0778fdfe8e76c6d2d76338bacad5d041
nguyen.thanh.xuan@kloon.vn <nguyen.thanh.xuan@kloon.vn>
2017-12-25 19:05:17 +0700

    Comit code

---
+++ ShopCoreApp/wwwroot/app/controllers/login/index.js
@@ -0,0 +1,39 @@
+﻿var loginController = (function () {
+    var initialize = function () {
+
+    };
+
+    var registerEvent = function () {
+        $('#btnSubmit').on('click', function () {
+            var userName = $('#txtUserName').val();
+            var password = $('#txtPassword').val();
+            loginController.login(userName, password, function (response) {
+
+            }, function (error) {
+
+            });
+        });
+    };
+
+    var login = function (user, pass, successCallBack, errorCallback) {
+        $.ajax({
+            type: 'POST',
+            data: {
+                username: user,
+                password: pass
+            },
+            dataType: 'json',
+            url: '/admin/login/authen',
+            success: successCallBack,
+            errorCallback: errorCallback
+        });
+    };
+    return {
+        initialize: initialize,
+        login: login,
+        registerEvent: registerEvent
+    };
+})();
+
+
+
