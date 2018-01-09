247df8fa2785ab3d650c608644d1ce5e3f24746a
thanhxuanhd007@gmail.com <thanhxuanhd007@gmail.com>
2018-01-09 18:20:55 +0700

    Product Categories Tree

---
+++ ShopCoreApp/Areas/Admin/Controllers/ProductCategoriesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShopCoreApp.Service.Interfaces;
+
+namespace ShopCoreApp.Areas.Admin.Controllers
+{
+    public class ProductCategoriesController : BaseController
+    {
+        #region Variables
+        private readonly IProductCategoryService _productCategoryService;
+        #endregion
+
+        #region Ctor
+        public ProductCategoriesController(IProductCategoryService productCategoryService)
+        {
+            _productCategoryService = productCategoryService;
+        }
+        #endregion
+
+        #region View
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #endregion
+        #region AJAX Action
+        public IActionResult GetAll() {
+
+            var productCategories = _productCategoryService.GetAll();
+            return new OkObjectResult(productCategories);
+        }
+
+        #endregion
+    }
+}
---
+++ ShopCoreApp/wwwroot/app/controllers/product-category/index.js
@@ -0,0 +1,44 @@
+﻿var productCategoriesController = (function () {
+    var initailize = function () {
+        loadData();
+        registerEvent();
+    };
+
+    var registerEvent = function () {
+    };
+
+    var loadData = function (isPageChanged) {
+        var template = $('#table-template').html();
+        var render = "";
+        $.ajax({
+            type: 'GET',
+            url: '/admin/productCategories/GetAll',
+            dataType: 'json',
+            success: function (response) {
+                if (response) {
+                    var data = [];
+                    $.each(response, function (i, item) {
+                        data.push({
+                            id: item.Id,
+                            text: item.Name,
+                            parentId: item.ParentId,
+                            sortOrder: item.SortOrder
+                        });
+                    });
+                    var treeArray = app.unflattern(data);
+                    $('#treeProductCategories').tree({
+                        data: treeArray,
+                    });
+                }
+            },
+            error: function (error) {
+                console.log(error);
+                app.notify('Cannot loading data', 'error');
+            }
+        });
+    };
+    return {
+        initailize: initailize,
+        registerEvent: registerEvent
+    };
+})();
